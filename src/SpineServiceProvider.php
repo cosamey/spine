3af5edf2e2f4c87b5819e81f7eba50c04e546817
Chris Teyerl <chris.teyerl@cosamey.com>
2026-01-05 16:23:11 +0100

    Add default configuration

--- src/SpineServiceProvider.php
+++ src/SpineServiceProvider.php
@@ -19,4 +19,48 @@ class SpineServiceProvider extends PackageServiceProvider
             ->hasMigration('update_users_table')
             ->runsMigrations();
     }
+
+    public function packageBooted(): void
+    {
+        $this->configureCommands();
+        $this->configureDefaults();
+        $this->configureModels();
+        $this->configureUrls();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureDefaults(): void
+    {
+        Number::useCurrency(config()->string('app.currency', 'EUR'));
+        Number::useLocale(config()->string('app.locale', 'en_US'));
+    }
+
+    private function configureModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+        Model::shouldBeStrict(app()->isLocal());
+
+        /** @var array<string, class-string<Model>> $morphMap */
+        $morphMap = collect(File::files(app_path('Models')))
+            ->mapWithKeys(function (\SplFileInfo $file): array {
+                $fileName = $file->getBasename('.php');
+                $namespace = 'App\\Models\\'.$fileName;
+
+                return is_subclass_of($namespace, Model::class)
+                    ? [str($fileName)->snake()->toString() => $namespace]
+                    : [];
+            })
+            ->toArray();
+
+        Relation::enforceMorphMap($morphMap);
+    }
+
+    private function configureUrls(): void
+    {
+        URL::forceHttps(app()->isProduction());
+    }
 }
